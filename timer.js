f4593faf12ac17821d4f3fd6497ef919cd0f8e76
James Wetton <30812961+James-Wetton@users.noreply.github.com>
2018-01-17 20:48:53 +1100

    Working timer in terminal (#3)

---
+++ timer.js
@@ -0,0 +1,18 @@
+var timer = 180;
+var delayInMilliseconds = 1000;
+var interval = 0;
+
+interval = setInterval(function() {
+    /*
+    if (timer => 165){
+       console.log("autonomous")
+    } if (timer < 165){
+        console.log("teleop")
+    }
+    */
+    //Uncomment the above code for auto and teleop code
+    console.log('\033c');
+    console.log(timer--);
+}, delayInMilliseconds);
+
+// countdown timer
