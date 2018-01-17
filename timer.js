656060b7cf5b25c7ca98a0f65488ec14d85dbb6f
James-Wetton <james.wetton@users.noreply.github.com>
2018-01-17 19:49:50 +1100

    Added better timer and added functionality to clear previous log of timer.

--- timer.js
+++ timer.js
@@ -1,15 +1,18 @@
 var timer = 180;
+var delayInMilliseconds = 1000;
+var interval = 0;
 
-
-
-while (timer > 0) {
+interval = setInterval(function() {
+    /*
     if (timer => 165){
-        console.log(autonomous)
+       console.log("autonomous")
     } if (timer < 165){
-        console.log(teleop)
+        console.log("teleop")
     }
-    //wait for one second
-    console.log(timer --)
-}
+    */
+    //Uncomment the above code for auto and teleop code
+    console.log('\033c')
+    console.log(timer --);
+}, delayInMilliseconds);
 
 // countdown timer
