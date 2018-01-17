4393730da602c8108be657a8494149cf1c2c454c
James Wetton <30812961+James-Wetton@users.noreply.github.com>
2018-01-17 15:44:43 +1100

    Updated timer.js
    removed sleeping function

--- timer.js
+++ timer.js
@@ -1,21 +1,15 @@
 var timer = 180;
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-}
 
-while (timer > 0) {
+
+while (*some form of timer* > 0) {
     if (timer => 165){
         console.log(autonomous)
     } if (timer < 165){
         console.log(teleop)
     }
-    sleep(1000)
+    *wait for one second*
     console.log(timer --)
 }
+// somebody with js experience please add the timers
 // countdown timer
