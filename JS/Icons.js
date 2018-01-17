aa49268f2b8e8d989e18a27e8e68331f45d1dfca
Winston Tran <winston.tran@thedropbears.org.au>
2018-01-17 16:23:31 +1100

    Errors in Icons.js

--- JS/Icons.js
+++ JS/Icons.js
@@ -1,14 +1,14 @@
 var Cube = false;
 var Scale = false;
-if(Cube=false){
+if(Cube==false){
     document.getElementById('cube') = "no_cube.png";
 }
-if(Cube=true){
+if(Cube==true){
     document.getElementById('cube') = "cube.png";
 }
-if(Scale = false){
+if(Scale == false){
     document.getElementById('scale') = "no_scale.png";
 }
-if(Scal = true){
+if(Scal == true){
     document.getElementById('scale') = "scale.png"
 }
