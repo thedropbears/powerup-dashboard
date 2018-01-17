1d156366c130778d98e9a48deb0729feb3a484ed
Winston Tran <winston.tran@thedropbears.org.au>
2018-01-17 16:21:49 +1100

    Changes in files

--- JS/Checklist.js
+++ JS/Checklist.js
@@ -11,37 +11,37 @@ var Task10 = "";
 
 var TaskNm = 0;
 function NextT(){
-    if(TaskNm = 0){
+    if(TaskNm == 0){
         document.getElementById("CL").innerHTML = Task1;
     }
-    if(TaskNm = 1){
+    if(TaskNm == 1){
         document.getElementById("CL").innerHTML = Task2;
     }
-    if(TaskNm = 2){
+    if(TaskNm == 2){
         document.getElementById("CL").innerHTML = Task3;
     }
-    if(TaskNm = 3){
+    if(TaskNm == 3){
         document.getElementById("CL").innerHTML = Task4;
     }
-    if(TaskNm = 4){
+    if(TaskNm == 4){
         document.getElementById("CL").innerHTML = Task5;
     }
-    if(TaskNm = 5){
+    if(TaskNm == 5){
         document.getElementById("CL").innerHTML = Task6;
     }
-    if(TaskNm = 6){
+    if(TaskNm == 6){
         document.getElementById("CL").innerHTML = Task7;
     }
-    if(TaskNm = 7){
+    if(TaskNm == 7){
         document.getElementById("CL").innerHTML = Task8;
     }
-    if(TaskNm = 8){
+    if(TaskNm == 8){
         document.getElementById("CL").innerHTML = Task9;
     }
-    if(TaskNm = 9){
+    if(TaskNm == 9){
         document.getElementById("CL").innerHTML = Task10;
     }
     else{
-        return null
+        return null;
     }
 }
