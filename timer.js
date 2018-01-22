25a1364692485fd6c45449af0b7f88fba30dfbac
James-Wetton <james.wetton@users.noreply.github.com>
2018-01-22 15:33:58 +1100

    Added jquery code for use in dashboard.

--- timer.js
+++ timer.js
@@ -5,17 +5,24 @@ var interval = 0;
 var refresh = setInterval(timer_app, delayInMilliseconds);
 
 function timer_app(){
-    console.log('\033c');
+    //console.log('\033c');
     if (timer <= 0){
-        console.log("END GAME");
+        //console.log("END GAME");
+        document.getElementById("MTimer").textContent="END GAME";
         clearInterval(refresh);
     }
+    
     if (timer <= 119){
-        console.log(timer-- + " TELEOP");
+        //console.log(timer-- + " TELEOP");
+        //console.log(timer--);
+        document.getElementById("MTimer").textContent=timer;
     }
     if (timer >= 120){
-        console.log(timer-- + " AUTONOMOUS");
+        //console.log(timer-- + " AUTONOMOUS");
+        //console.log(timer--);
+        document.getElementById("MTimer").textContent=timer;
     }
-
+    
+}
 
 // countdown timer
