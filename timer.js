0185ad8d9defee19a30c79d7fb8c5eb8a28fdb1c
James-Wetton <james.wetton@users.noreply.github.com>
2018-01-22 16:10:52 +1100

    Fix jquery to work with id in html.

--- timer.js
+++ timer.js
@@ -3,24 +3,32 @@ var timer = 135;
 var delayInMilliseconds = 1000;
 var interval = 0;
 var refresh = setInterval(timer_app, delayInMilliseconds);
+var text = document.createTextNode("timer here");
+var span = document.getElementById("MTimer");
 
 function timer_app(){
     //console.log('\033c');
     if (timer <= 0){
         //console.log("END GAME");
-        document.getElementById("MTimer").textContent=timer;
+        //document.getElementById("MTimer").textContent=timer;
+        text = document.createTextNode(timer);
+        span.appendChild(text);
         clearInterval(refresh);
     }
     
     if (timer <= 119){
         //console.log(timer-- + " TELEOP");
         //console.log(timer--);
-        document.getElementById("MTimer").textContent=timer;
+        //document.getElementById("MTimer").textContent=timer;
+        text = document.createTextNode(timer);
+        span.appendChild(text);
     }
     if (timer >= 120){
         //console.log(timer-- + " AUTONOMOUS");
         //console.log(timer--);
-        document.getElementById("MTimer").textContent=timer;
+        //document.getElementById("MTimer").textContent=timer;
+        text = document.createTextNode(timer);
+        span.appendChild(text);
     }
     
 }
