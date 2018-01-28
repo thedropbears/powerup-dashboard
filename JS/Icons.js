91477e4363f8376d37b56e972da0886e6275e848
Winston Tran <winston.tran@thedropbears.org.au>
2018-01-28 15:24:53 +1100

    Error Fix

--- JS/Icons.js
+++ JS/Icons.js
@@ -1,27 +1,27 @@
 
 function Icons(){
-if(Scale == true){
+if(Scale === true){
 	document.getElementById('Scale').src="Images/Our_Scale.png";
 }
-else if(Scale == false){
+else if(Scale === false){
 	document.getElementById('Scale').src="Images/No_Scale.png";
 }
-if(Cube == true){
+if(Cube === true){
 	document.getElementById('Cube').src="Images/cube.png";
 }
-else if(Cube == false){
+else if(Cube === false){
 	document.getElementById('Cube').src="Images/no_cube.png";
 }
-if(Arrow == true){
+if(Arrow === true){
 	document.getElementById('Arrows').src="Images/up.png";
 }
-else if(Arrow == false){
+else if(Arrow === false){
 	document.getElementById('Arrows').src="Images/§§§.png";
 }
-if(DArrow == true){
+if(DArrow === true){
 	document.getElementById('DArrow').src="Images/down.png";
 }
-else if(Darrow == false){
+else if(Darrow === false){
 	document.getElementById('DArrow').src="Images/§§§.png";
 }
 }
