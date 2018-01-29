ecf9cf5290654be0f085c53a2910f400f257ee2d
Winston Tran <winston.tran@thedropbears.org.au>
2018-01-29 15:03:27 +1100

    Add Jame's timer.?

---
+++ timer.js
@@ -0,0 +1,40 @@
+//"use strict";
+var timer = 135;
+var delayInMilliseconds = 1000;
+var interval = 0;
+var refresh = setInterval(timer_app, delayInMilliseconds);
+var text = document.createTextNode("timer here");
+var span = document.getElementById("MTimer");
+
+function timer_app(){
+    //TODO receive start signal through network tables
+    
+    //console.log('\033c');
+    if (timer <= 0){
+        //console.log("END GAME");
+        //document.getElementById("MTimer").textContent=timer;
+        timer = timer--
+        text = document.createTextNode(timer);
+        span.appendChild(text);
+        clearInterval(refresh);
+    }
+    
+    if (timer <= 119){
+          //console.log(timer--);
+        //document.getElementById("MTimer").textContent=timer;
+        timer = timer--
+        text = document.createTextNode(timer);
+        span.appendChild(text);
+    }
+    if (timer >= 120){
+        //console.log(timer-- + " AUTONOMOUS");
+        //console.log(timer--);
+        //document.getElementById("MTimer").textContent=timer;
+        timer = timer--
+        text = document.createTextNode(timer);
+        span.appendChild(text)
+    }
+    
+}
+
+// countdown timer
