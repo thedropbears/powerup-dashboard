1ffc51f1d73985e36b16847fbfd672e27ccce6a7
Winston Tran <winston.tran@thedropbears.org.au>
2018-02-03 06:50:56 +1100

    id change for auto selctor

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     NetworkTables.addGlobalListener(onValueChanged, true);
 
     // hook up our SendableChoosers to combo boxes
-    attachSelectToSendableChooser("#selector", "/SmartDashboard/Autonomous Mode");
+    attachSelectToSendableChooser("#auto-selector", "/SmartDashboard/Autonomous Mode");
 });
 
 function resetGyro() {
